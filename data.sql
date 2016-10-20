223b6772536b21d49a3ccf91123a01757be6b7c5
Song Tian <stian3@cs.ualberta.ca>
2016-10-20 13:46:36 -0600

    completed data

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 INSERT INTO staff VALUES
-('100','D','Stewie Griffin', 'stewie','iwantmymommy'),
+('100','D','Doctor Who', 'jammydodger','iwantmywife'),
 ('101','D','Peter Griffin', 'peterpumpkineater','clam'),
 ('102','D','Chris Griffin', 'popsicles','1234'),
 ('103','D','Meg Griffin', 'pinkhat','0000'),
@@ -8,7 +8,7 @@ INSERT INTO staff VALUES
 ('125','N','Patrick Star','icantseemyforehead','duuuh'),
 ('126','N','Eugene Krabs','moneeey','pennypincher'),
 ('127','N','Sandy Cheeks','acornmuncher','hiya'),
-('200','A','Doctor Who','ihatedaleks','41625'),
+('200','A','Lois Griffin','ihatedaleks','41625'),
 ('201','A','Amelia Pond','thegirlwhowaited','41625'),
 ('202','A','Rory Roman','iheartamy','41625'),
 ('203','A','River Song','spoilers','41625');
@@ -29,23 +29,64 @@ INSERT INTO charts Values
 ('002','10002', '2010-02-22', '2010-03-16'),
 ('003','10003', '2011-01-16',NULL),
 ('004','10004', '2011-04-16',NULL),
-('005','10005', '2011-06-16', '2010-07-05'),
-('006','10006', '2012-06-16', '2001-01-01'),
+('005','10005', '2011-06-16', '2012-07-05'),
+('006','10006', '2011-06-16', '2012-01-01'),
 ('007','10007', '2011-12-24',NULL),
 ('008','10000', '2011-10-02', '2011-10-16'),
 ('009','10001', '2011-06-06',NULL);
 
 INSERT INTO symptoms VALUES
-('10000', '000','123', '2010-10-02','headache' ),
-('10000', '000','124', '2010-10-03','stomachache' ),
-('10001', '001','125', '2010-06-06','stomachache' ),
-('10001', '001','125', '2010-06-07','stomachache' ),
-('10001', '001','125', '2010-06-09','stomachache' ),
-('10002', '002','125', '2010-02-23','stomachache' ),
-('10003', '003','100', '2010-10-03','stomachache' ),
-('10003', '003','103', '2010-10-03','stomachache' ),
-('10004', '004','123', '2010-10-03','stomachache' ),
-('10005', '005','123', '2010-10-03','stomachache' ),
-('10006', '006','125', '2010-10-03','stomachache' ),
-('10007', '007','124', '2010-10-03','stomachache' ),
-('10007', '000','125', '2010-10-03','stomachache' );
+('10000', '000','123', '2010-10-02','uncontrollable gas' ),
+('10000', '008','124', '2011-10-03','stomachache' ),
+('10001', '001','125', '2010-06-06','cough' ),
+('10002', '002','125', '2010-02-28','headache' ),
+('10004', '004','125', '2010-05-09','cough' ),
+('10002', '002','125', '2010-02-25','diarrhea' ),
+('10002', '002','100', '2010-02-25','stomachache' ),
+('10003', '003','103', '2011-01-22','uncontrollable gas' ),
+('10004', '004','123', '2011-04-25','uncontrollable gas' ),
+('10005', '005','123', '2011-07-01','stomachache' ),
+('10006', '006','125', '2011-06-16','diarrhea' ),
+('10007', '007','124', '2011-12-27','uncontrollable gas' ),
+('10007', '007','125', '2011-12-25','stomachache' );
+
+INSERT INTO diagnoses VALUES
+('10000','000','100','2010-10-03','cancer'),
+('10001','001','101','2010-06-07','diabetes'),
+('10002','002','103','2010-03-07','malaria'),
+('10003','003','102','2011-01-25','worms'),
+('10004','004','102','2011-04-26','cancer'),
+('10005','005','100','2011-07-08','worms'),
+('10006','006','103','2011-06-17','food poisoning');
+
+INSERT INTO drugs VALUES 
+('Aspirin', 'Pain killers'),
+('Vicodin', 'Pain killers'),
+('Ibuprofen', 'Pain killers'),
+('Xanax', 'Tranquillizer'),
+('Diazepan', 'Tranquillizer'),
+('Hemp', 'Recreational'),
+('Cocaine', 'Recreational');
+
+INSERT INTO medications VALUES
+('10000','000','100','2010-10-03','2010-10-03','2010-11-03',3,'Cocaine'),
+('10001','001','102','2010-06-07','2010-06-07','2010-07-07',10,'Xanax'),
+('10002','002','100','2010-03-07','2010-03-07','2010-04-07',300,'Ibuprofen'),
+('10003','003','103','2011-01-25','2011-01-25','2011-01-30',1,'Hemp'),
+('10004','004','102','2011-04-26','2011-04-26','2011-05-15',7,'Hemp'),
+('10006','006','101','2011-06-17','2011-06-17','2011-06-20',50,'Aspirin');
+
+INSERT INTO reportedallergies VALUES 
+('10000','Vicodin'),
+('10003','Diazepan'),
+('10005','Aspirin');
+
+INSERT INTO inferredallergies VALUES
+('Vicodin','Ibuprofen'),
+('Cocaine','Xanax');
+
+
+
+
+
+
