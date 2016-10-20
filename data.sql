61ad25f269c47ef8aff85d6d6decc71162568ed3
Song Tian <stian3@cs.ualberta.ca>
2016-10-20 12:27:29 -0600

    forgot data file

---
+++ data.sql
@@ -0,0 +1,53 @@
+INSERT INTO staff VALUES(
+('100','D','Stewie Griffin', 'stewie','iwantmymommy'),
+('101','D','Peter Griffin', 'peterpumpkineater','clam'),
+('102','D','Chris Griffin', 'popsicles','1234'),
+('103','D','Meg Griffin', 'pinkhat','0000'),
+('123','N','Sheldon Cooper','physicsnerd','amy'),
+('124','N','Spongebob Squarepants','frycook','patties'),
+('125','N','Patrick Star','icantseemyforehead','duuuh'),
+('126','N','Eugene Krabs','moneeey','pennypincher'),
+('127','N','Sandy Cheeks','acornmuncher','hiya'),
+('200','A','Doctor Who','ihatedaleks','41625'),
+('200','A','Amelia Pond','thegirlwhowaited','41625'),
+('200','A','Rory Roman','iheartamy','41625'),
+('200','A','River Song','spoilers','41625'))
+
+Insert Into patients VALUES(
+('10000', 'Dalek', '18-35', "1414 Edmonton, Alberta", "7801001000", '7809999966'),
+('10001', 'Silence', '0-18', "1313 Edmonton, Alberta", "7801001001", '7809996666'),
+('10002', 'Angel', '0-18', "1515 Edmonton, Alberta", "7801001002", '7806666999'),
+('10003', 'Tardis', '45-60', "1616 Edmonton, Alberta", "7801001003", '7801111444'),
+('10004', 'Melody Pond', '45-60', "1717 Edmonton, Alberta", "7801001004", '7804444111'),
+('10005', 'Prisoner Zero', '0-18', "1818 Calgary, Alberta", "7801001005", '7804545131'),
+('10006', 'Pandorica', '18-35', "1919 Calgary, Alberta", "7801001006", '7805555555'),
+('10007', 'Silurian', '45-60', "1919 Calgary, Alberta", "7801001007", '7805555464'))
+
+INSERT INTO charts Values(
+('000','10000', '2010-10-02', '2010-10-16'),
+('001','10001', '2010-06-06', '2010-06-17'),
+('002','10002', '2010-02-22', '2010-03-16'),
+('003','10003', '2011-01-16'),
+('004','10004', '2011-04-16'),
+('005','10005', '2011-06-16', '2010-07-05'),
+('006','10006', '2012-06-16', '2001-01-01'),
+('007','10007', '2011-12-24')
+('008','10000', '2011-10-02', '2011-10-16'),
+('009','10001', '2011-06-06'))
+
+INSERT INTO symptoms VALUES(
+('10000', '000','123', '2010-10-02','headache' ),
+('10000', '000','124', '2010-10-03','stomachache' ),
+('10001', '001','125', '2010-06-06','stomachache' ),
+('10001', '001','125', '2010-06-07','stomachache' ),
+('10001', '001','125', '2010-06-09','stomachache' ),
+('10002', '002','125', '2010-02-23','stomachache' ),
+('10003', '003','100', '2010-10-03','stomachache' ),
+('10003', '003','103', '2010-10-03','stomachache' ),
+('10004', '004','123', '2010-10-03','stomachache' ),
+('10005', '005','123', '2010-10-03','stomachache' ),
+('10006', '006','125', '2010-10-03','stomachache' ),
+('10007', '007','124', '2010-10-03','stomachache' ),
+('10007', '000','125', '2010-10-03','stomachache' ),
+
+)
