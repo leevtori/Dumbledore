9129378b247eecc54931605882b942453cbe47d6
Song Tian <stian3@cs.ualberta.ca>
2016-10-27 16:06:49 -0600

    updated admin.py

--- admin.py
+++ admin.py
@@ -2,15 +2,13 @@
 import sqlite3
 import re
 
-conn = sqlite3.connect('hospital.db')
+conn = sqlite3.connect("hospital.db")
 c = conn.cursor()
 #---------------------------------------------------------------  
 def admin_query1():
-    print('List for each doctor the name and the total amount of each drug that the doctor prescribed in a specified period of time.')
+    print("List for each doctor the name and the total amount of each drug that the doctor prescribed in a specified period of time.\n")
 
     from_date = "" 
-    to_date = " "
-
     #regular expression from http://www.regular-expressions.info/dates.html
     while not re.match('^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$',from_date):
         from_date = raw_input("Please indicate START DATE with format 'yyyy-mm-dd' (e.g. 2010-01-31): ")
@@ -22,23 +20,27 @@ def admin_query1():
     c.execute("select staff.name, medications.drug_name,SUM(medications.amount) AS total_amount_prescribed from staff, medications where datetime(medications.mdate)>=datetime(:from_date) and datetime(medications.mdate)<=datetime(:to_date) and medications.staff_id=staff.staff_id GROUP BY medications.drug_name;",{"from_date":from_date, "to_date":to_date})
 
     result_list = c.fetchall()
+    print "Doctor  |Drug  |Total Amount"
+    print "--------------------------------"
     for i in range(len(result_list)):
-        print(result_list[i])
+        s = []
+        for j in range(len(result_list[i])):
+            s.append(str(result_list[i][j]))
+        print '| '.join(s)
 
     print('')
     print('')
 
 #---------------------------------------------------------------  
 def admin_query2():
-    print("For each category of drugs, list the total amount prescribed for each drug in that category in a specified period of time.")
+    
+    print("For each category of drugs, list the total amount prescribed for each drug in that category in a specified period of time.\n")
 
     from_date = "" 
-    to_date = " "
-
     #regular expression from http://www.regular-expressions.info/dates.html
     while not re.match('^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$',from_date):
         from_date = raw_input("Please indicate START DATE with format 'yyyy-mm-dd' (e.g. 2010-01-31): ")
-
+       
     to_date = ""
     while not re.match('^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$',to_date):
         to_date = raw_input("Please indicate END DATE with format 'yyyy-mm-dd'(e.g. 2010-02-20): ")
@@ -46,38 +48,59 @@ def admin_query2():
     c.execute("select drugs.category, medications.drug_name, SUM(medications.amount) AS total_amount_prescribed from drugs, medications where medications.drug_name=drugs.drug_name and datetime(medications.mdate)>=datetime(:from_date) and datetime(medications.mdate)<=datetime(:to_date) GROUP BY drugs.category,medications.drug_name;",{"from_date":from_date, "to_date":to_date})
 
     result_list = c.fetchall()
+    print "Category  |Drug  |Total Amount"
+    print "--------------------------------"
     for i in range(len(result_list)):
-        print(result_list[i])
+        s = []
+        for j in range(len(result_list[i])):
+            s.append(str(result_list[i][j]))
+        print '| '.join(s)
 
     print('')
     print('')
 
 #---------------------------------------------------------------  
 def admin_query3():
+
+    print("List for a given diagnosis all possible medications that have been prescribed over time after that diagnosis.\n")
+
     diagnosis=" "
     while diagnosis.isalpha() == False:
         diagnosis = raw_input("Enter diagnosis name: ")
+    diagnosis=diagnosis.lower()
 
-    c.execute("select medications.drug_name as possible_medications from medications, diagnoses where diagnoses.diagnosis= :diagnosis and julianday(medications.mdate)>=julianday(diagnoses.ddate) group by medications.drug_name order by count(*) DESC;",{"diagnosis":diagnosis})
+    c.execute("select medications.drug_name as possible_medications from medications, diagnoses where lower(diagnoses.diagnosis)= :diagnosis and julianday(medications.mdate)>=julianday(diagnoses.ddate) group by medications.drug_name order by count(*) DESC;",{"diagnosis":diagnosis})
     
     result_list = c.fetchall()
+    print "Possible Medications"
+    print "--------------------------------"
     for i in range(len(result_list)):
-        print(result_list[i])
-
+        s = []
+        for j in range(len(result_list[i])):
+            s.append(str(result_list[i][j]))
+        print '| '.join(s)
     print('')
     print('')
 
 #---------------------------------------------------------------        
 def admin_query4():
+    print("List for a given drug all the diagnoses that have been made before prescribing the drug.\n")
+
     drug=" "
     while drug.isalpha() == False:
         drug = raw_input("Enter drug name: ")
+    drug=drug.lower()
 
-    c.execute("select diagnoses.diagnosis as previous_diagnoses from diagnoses,medications where medications.drug_name=:drug and julianday(diagnoses.ddate)<julianday(medications.mdate) group by previous_diagnoses order by AVG(medications.amount);",{"drug":drug})
+    c.execute("select diagnoses.diagnosis as previous_diagnoses from diagnoses,medications where lower(medications.drug_name)=:drug and julianday(diagnoses.ddate)<julianday(medications.mdate) group by previous_diagnoses order by AVG(medications.amount);",{"drug":drug})
 
     result_list = c.fetchall()
+    print "Previous Diagnoses"
+    print "--------------------------------"
     for i in range(len(result_list)):
-        print(result_list[i])
+        s = []
+        for j in range(len(result_list[i])):
+            s.append(str(result_list[i][j]))
+        print '| '.join(s)
 
     print('')
     print('')
@@ -85,7 +108,6 @@ def admin_query4():
     
 
 #MAIN-----------------------------------------------------------
-
 
 
 
--- hospital.py
+++ hospital.py
@@ -1,23 +1,23 @@
 #from doctor import *
 from admin import *
 
-
+user = ['A',5]
 # commands:
-DOC = ['"search"', '"add"']
+DOC = ['search', 'add']
 NURSE = []
-ADMIN = ['"doctor_sum" -- For each doctor, list the total amount of each drug prescribed in a specified period of time.\n','"drug_sum" -- For each category of drugs, list the total amount prescribed for each drug in that category in a specified period of time.\n','"pos_med" -- List for a given diagnosis all possible medications that have been prescribed over time after that diagnosis','"pos_diag" -- List for a given drug all the diagnoses that have been made before prescribing the drug']
+ADMIN = ['doctor_sum','drug_sum','pos_med','pos_diag']
 # user[0] = role
 # user[1] = staff id
 
 # After user logs in, continuously ask what to do until they log out.
 
-print "Available operations:",
+print "Available operations: ",
 if user[0] == "D":
-     print ', '.join(DOC)
+     print ' ,'.join(DOC)
 elif user[0] == "N":
-     print ', '.join(NURSE)
+     print ' ,'.join(NURSE)
 else:
-    print ', '.join(ADMIN)
+    print ' ,'.join(ADMIN)
 
 while True:
      op = raw_input("Please enter an operation or 'logout' to quit: ").lower()
@@ -32,20 +32,20 @@ while True:
                     line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ")
                     if line_type == '0':
                          break
-                         if line_type == '0':
-                              continue
+               if line_type == '0':
+                    continue
                               
-          add_line(line_type)
+               add_line(line_type)
 
-        elif op == 'logout':
-             print "Goodbye!"
-             break
+          elif op == 'logout':
+               print "Goodbye!"
+               break
 
-        else:
-             print "Invalid Command"
+          else:
+               print "Invalid Command"
 
-     #NURSE
-     elif user[0] == "A":
+     #ADMIN
+     if user[0] == "A":
           if op == "doctor_sum":
                admin_query1()
                
@@ -60,7 +60,8 @@ while True:
           elif op == "logout":
                print "Goodbye!"
                break
-
-     #NURSE
+          
+          #NURSE
      elif user[0] == "N":
           pass
+          
