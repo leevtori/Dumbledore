b971ac723aceccec1f766d6a6c7a388b825f3d25
Kirsten Kwong <kirstenkwong@gmail.com>
2016-10-27 22:56:52 -0600

    updated hospital.py

--- hospital.py
+++ hospital.py
@@ -1,66 +1,79 @@
 from authentication import *
 from doctor import *
 from admin import *
+from nurse import *
 
-# user[0] = role
-# user[1] = staff id
 
 # commands:
 DOC = ['search', 'add']
-NURSE = []
+NURSE = ['close_chart', 'create_chart']
 ADMIN = ['doctor_sum','drug_sum','pos_med','pos_diag']
 
 # After user logs in, continuously ask what to do until they log out.
-print "Available operations: ",
 if user[0] == "D":
-     print ','.join(DOC)
+    print "you are a doctor"
+    print "Available operations: ",
+    print ', '.join(DOC)
 elif user[0] == "N":
-     print ', '.join(NURSE)
+    print "You are a nurse"
+    print "Available operations: ",
+    print ', '.join(NURSE)
 else:
+    print "You are an admin"
+    print "Available operations: ",
     print ', '.join(ADMIN)
 
+
+
 while True:
-     op = raw_input("Please enter an operation or 'logout' to quit: ").lower()
-     #Doctors
-     if user[0] == "D":
-          if op == "search":
-               get_chart()
 
-          elif op == "add":
-               line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ").upper()
-               while line_type not in "SDM":
-                    line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ").upper()
-                    if line_type == '0':
-                         break
-               if line_type == '0':
+    op = raw_input("Please enter an operation or 'logout' to quit: ").lower()
+    #Doctors
+    if user[0] == "D":
+        if op == "search":
+            get_chart()
+        elif op == "add":
+            line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ").upper()
+            while line_type not in "SDM":
+                line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ")
+                if line_type == '0':
+                    break
+                if line_type == '0':
                     continue
+                add_line(line_type)
 
-               add_line(line_type)
+        elif op == 'logout':
+            print "Goodbye!"
+            break
 
-          elif op == 'logout':
-               print "Goodbye!"
-               break
+        else:
+            print "Invalid Command"
 
-          else:
-               print "Invalid Command"
+    #ADMIN
+    if user[0] == "A":
+        if op == "doctor_sum":
+            admin_query1()
 
-     #ADMIN
-     if user[0] == "A":
-          if op == "doctor_sum":
-               admin_query1()
+        elif op == "drug_sum":
+            admin_query2()
 
-          elif op == "drug_sum":
-               admin_query2()
+        elif op == "pos_med":
+            admin_query3()
 
-          elif op == "pos_med":
-               admin_query3()
+        elif op == "pos_diag":
+            admin_query4()
 
-          elif op == "pos_diag":
-               admin_query4()
-          elif op == "logout":
-               print "Goodbye!"
-               break
+        elif op == "logout":
+            print "Goodbye!"
+            break
 
           #NURSE
-     elif user[0] == "N":
-          pass
+    elif user[0] == "N":
+        if op == "close_chart":
+            hcno = raw_input("Please enter the patients Healthcare No: ")
+            nurse_q1(hcno)
+        elif op == "create_chart":
+            nurse_q2()
+        elif op == "logout":
+            print "Goodbye!"
+            break
