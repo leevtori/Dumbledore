8b3487bb9eee76891f1ae0bc57d0763877c98ee4
leevtori <vlee2@ualberta.ca>
2016-10-27 21:49:02 -0600

    nurse stuff

--- hospital.py
+++ hospital.py
@@ -4,67 +4,83 @@ from admin import *
 from nurse import *
 
 # user log in
-#user = None
-#while not user:
-#    user = authenticate()
-# user[0] = role
-# user[1] = staff id
+user = None
+while not user:
+    user = authenticate()
+    user[0] = role
+    user[1] = staff_id
 
 # commands:
 DOC = ['search', 'add']
-NURSE = []
+NURSE = ['close_chart', 'create_chart']
 ADMIN = ['doctor_sum','drug_sum','pos_med','pos_diag']
 
 # After user logs in, continuously ask what to do until they log out.
 print "Available operations: ",
 if user[0] == "D":
-     print ' ,'.join(DOC)
-elif user[1] == "N":
-     print ' ,'.join(NURSE)
+    print "you are a doctor"     
+    print ' ,'.join(DOC)
+elif user[0] == "N":
+    print "You are a nurse"     
+    print ' ,'.join(NURSE)
 else:
-     print ' ,'.join(ADMIN)
+    print "You are an admin"     
+    print ' ,'.join(ADMIN)
+     
 
 while True:
-     op = raw_input("Please enter an operation or 'logout' to quit: ").lower()
-     #Doctors
-     if user[0] == "D":
-          if op == "search":
-               get_chart()
-          elif op == "add":
-               line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ").upper()  
-               while line_type not in "SDM":
-                    line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ")
-                    if line_type == '0':
-                         break
-               if line_type == '0':
+    
+    op = raw_input("Please enter an operation or 'logout' to quit: ").lower()
+    #Doctors
+    if user[0] == "D":
+        if op == "search":
+            get_chart()
+        elif op == "add":
+            line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ").upper()  
+            while line_type not in "SDM":
+                line_type = raw_input("Enter line type ('S','D','M' or '0' to exit): ")
+                if line_type == '0':
+                    break
+                if line_type == '0':
                     continue
+                add_line(line_type)
+        
+        elif op == 'logout':
+            print "Goodbye!"
+            break
 
-               add_line(line_type)
+        else:
+            print "Invalid Command"
 
-          elif op == 'logout':
-               print "Goodbye!"
-               break
+    #ADMIN
+    if user[0] == "A":
+        if op == "doctor_sum":
+            admin_query1()
 
-          else:
-               print "Invalid Command"
+        elif op == "drug_sum":
+            admin_query2()
 
-     #ADMIN
-     if user[0] == "A":
-          if op == "doctor_sum":
-               admin_query1()
+        elif op == "pos_med":
+            admin_query3()
 
-          elif op == "drug_sum":
-               admin_query2()
-
-          elif op == "pos_med":
-               admin_query3()
-
-          elif op == "pos_diag":
-               admin_query4()
-          elif op == "logout":
-               print "Goodbye!"
-               break
+        elif op == "pos_diag":
+            admin_query4()
+             
+        elif op == "logout":
+            print "Goodbye!"
+            break
 
           #NURSE
-     elif user[0] == "N":
-          pass
+    elif user[0] == "N":
+        if op == "close_chart":
+            hcno = raw_input("Please enter the patients Healthcare No: ")
+            nurse_q1(hcno)
+        elif op == "create_chart":
+            nurse_q2()
+        elif op == "logout":
+            print "Goodbye!"
+            break
+     
+     
+     
+     
--- nurse.py
+++ nurse.py
@@ -6,10 +6,14 @@ c = conn.cursor()
 
 # Nurses tasks
 
-def nurse_q1(c, connection):
+def nurse_q1(hcno):
+    print "This is the close chart"
 
     print 'If you are closing a patient file, please enter patients Healthcare Number'
     hcno = raw_input('Enter hcno: ')
     
     c.execute('UPDATE charts SET edate = datetime() WHERE edate is NULL;') 
     
+    
+def nurse_q2():
+    print "This is the creat chart"
